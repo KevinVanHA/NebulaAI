8c974790f864fd1be2dbd5506e692319267ed93b
Kevin Van Haesendonck <118561655+KevinVanHA@users.noreply.github.com>
2025-03-17 21:44:25 -0500

    UpdatedHomepage

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -20,9 +20,6 @@ const blockchains = [
   { id: "56", name: "BNB Smart Chain Mainnet" }, // BNB Smart Chain Mainnet
   { id: "137", name: "Polygon Mainnet" }, // BNB Smart Chain Mainnet
 
-  // Testnets
-  { id: "11155111", name: "Ethereum Sepolia" }, // Ethereum Sepolia
-  { id: "97", name: "BNB Smart Chain" }, // BNB Smart Chain
 ];
 
 export function Hero() {
@@ -44,7 +41,7 @@ export function Hero() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl mb-8">
           <span className="block">EAC Blockchain Explorer</span>
-          <span className="block text-indigo-600">Powered by EVil Alien Collection</span>
+          <span className="block text-indigo-600">Powered by Evil Alien Collection</span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-8">
           Enter any contract address, and get a detailed analysis, functions,
